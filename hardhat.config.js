ad9374daee41ce89486b0177be6ea472e17a888c
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-07-23 22:48:28 +0100

    fix: reconfigured file

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,6 @@
-require("@nomicfoundation/hardhat-toolbox");
-
 /** @type import('hardhat/config').HardhatUserConfig */
+require('@nomicfoundation/hardhat-chai-matchers')
+require('@nomiclabs/hardhat-ethers')
 module.exports = {
   solidity: "0.8.28",
 };
