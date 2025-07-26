d0b42b2ee1042acb51aeffb2a7e59f53606997e6
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-07-26 12:45:54 +0100

    fix: hardhat config error

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,22 @@
 /** @type import('hardhat/config').HardhatUserConfig */
 require('@nomicfoundation/hardhat-chai-matchers')
 require('@nomiclabs/hardhat-ethers')
+require('@nomicfoundation/hardhat-verify')
+require('dotenv').config()
+
+
 module.exports = {
   solidity: "0.8.28",
+  networks: {
+    sepolia: {
+      url: process.env.SEPOLIA_RPC_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`]
+    }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
+  },
+  sourcify: {
+    enabled: true
+  }
 };
