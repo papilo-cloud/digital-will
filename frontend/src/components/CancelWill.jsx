b309151de5f8a797d59c719578d09b71ec552a1b
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-07-28 03:39:21 +0100

    feat: add CancelWill component to allow testators cancel active Wills

---
+++ frontend/src/components/CancelWill.jsx
@@ -0,0 +1,64 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { useContract } from '../context/ContractContext'
+import { ethers } from 'ethers'
+import { toast } from 'react-toastify'
+
+const CancelWill = () => {
+    const [testatorAddress, setTestatorAddress] = useState('')
+    const [loading, setLoading] = useState(false)
+    const [isTestator, setIsTestator] = useState(false)
+
+    const {contract, walletAddress} = useContract()
+
+    useEffect(() => {
+      const checkIfTestator = async () => {
+        if(!contract || !walletAddress) return;
+        try {
+          const will = await contract.usersWill(walletAddress)
+          setIsTestator(will?.beneficiaries?.length > 0);
+        } catch (err) {
+          console.error(err);
+          setIsTestator(false)
+        }
+      }
+      checkIfTestator()
+    }, [contract, walletAddress])
+
+    const handleCancel = async () => {
+        if (!contract) {
+            toast.warn('Contract not connected.')
+            return;
+        }
+        if (!isTestator) {
+            toast.warn('You are not the owner of any will')
+            return;
+        }
+        try {
+          setLoading(true)
+            const tx = await contract.cancelWill()
+            await tx.wait()
+            toast.success("Will cancelled and funds refunded!")
+        } catch (err) {
+          console.error(err);
+          toast.error('Failed to cancel Will.')
+        } finally {
+          setLoading(false)
+        }
+    }
+    
+  return (
+    <div className='w-full mx-auto mt-10 bg-[#151515] p-6 rounded-xl shadow-md'>
+        <h2 className='text-lg font-semibold mb-4 text-[#ccc]'>Cancel Will</h2>
+        <button
+          onClick={handleCancel}
+          disabled={loading}
+          className='bg-red-500 cursor-pointer text-[#ccc] px-4 py-2 rounded hover:bg-red-700 w-full'
+        >
+          {loading ? 'Cancelling...' : 'Cancel Will'}
+        </button>
+    </div>
+  )
+}
+
+export default CancelWill
