c09f983b9ab18c7b662fcff7cfb4405049c2ce22
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-07-29 00:07:38 +0100

    feat(utils): implement countDownTimer hook for dynamic countdown based on timestamps

---
+++ frontend/src/utils/CountdownTimer.jsx
@@ -0,0 +1,32 @@
+import { BigNumber } from "ethers"
+import { useEffect, useState } from "react"
+import formatTime from "./formatTime"
+
+const CountdownTimer = ({lastPing, deathTimeout}) => {
+    const [timeleft, setTimeLeft] = useState(0)
+
+    useEffect(() => {
+        const lastPingTime = BigNumber.from(lastPing).toNumber()
+        const timeOut = BigNumber.from(deathTimeout).toNumber()
+        const deathTimeStamp = lastPingTime + timeOut
+
+        const updateCountdown = () => {
+            const now = Math.floor(Date.now() / 1000)
+            const remaining = deathTimeStamp - now
+            setTimeLeft(remaining > 0 ? remaining : 0)
+        }
+
+        updateCountdown()
+        const interval = setInterval(updateCountdown, 1000)
+
+        return () => clearInterval(interval)
+    }, [lastPing, deathTimeout])
+
+    return (
+        <div className="text-lg font-semibold text-[#fff] text-center mt-2">
+            Will executable in: {formatTime(timeleft)}
+        </div>
+    )
+}
+
+export default CountdownTimer
