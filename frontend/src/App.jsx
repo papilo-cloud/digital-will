6865b51746851da5829acfa767e7517b1090a053
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-07-29 12:00:48 +0100

    feat(ui): add routing logic to switch between Create, View, and Execute Will views

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,27 @@
-import { useState } from 'react'
+import { ContractProvider } from './context/ContractContext'
+import CreateWillForm from './components/WillForm'
+import ExecuteWill from './components/ExecuteWill'
+import { ToastContainer } from 'react-toastify'
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/DashBoard'
+import NotFound from './pages/NotFound'
+import MyWill from './components/MyWill'
 
 function App() {
 
   return (
-    <h1>Hello world</h1>
+    <ContractProvider>
+        <ToastContainer position='top-right' autoClose={4000} />
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Dashboard />} />
+            <Route path='/create' element={<CreateWillForm />} />
+            <Route path='/execute' element={<ExecuteWill />} />
+            <Route path='/mywill' element={<MyWill /> } />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </BrowserRouter>
+    </ContractProvider>
   )
 }
 
