104c25891490d2783367cc04f8d6fa54f92deb87
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-07-31 04:39:28 +0100

    style: update style

--- frontend/src/components/Card/DashboardCard.jsx
+++ frontend/src/components/Card/DashboardCard.jsx
@@ -2,8 +2,8 @@ const DashboardCard = ({ title, value }) => {
     
   return (
     <div className="bg-[#151515] rounded-xl shadow-xl p-4 w-full">
-      <h3 className="text-lg text-[#ccc]">{title}</h3>
-      <p className="text-2xl font-bold text-white">{value}</p>
+      <h3 className="text-lg text-white mb-4">{title}</h3>
+      <p className="text-2xl font-bold text-gray-300">{value}</p>
     </div>
   );
 };
