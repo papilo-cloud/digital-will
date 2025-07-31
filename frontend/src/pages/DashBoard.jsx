dbfb709adc7bb1087f331446632d31939b53d5d2
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-07-31 04:48:42 +0100

    update: add the Cards components and integrate useGetWills hook

--- frontend/src/pages/DashBoard.jsx
+++ frontend/src/pages/DashBoard.jsx
@@ -1,69 +1,34 @@
-import { useEffect, useState } from "react";
-import DashboardCard from "../components/DashboardCard";
 import DashboardLayout from "../components/DashboardLayout";
-import { useContract } from "../context/ContractContext";
-import contractAbi from '../abi/CreateWill.json'
-import { Contract, ethers } from "ethers";
-import { contractAddress } from "../utils/contractAddress";
+import DashboardCard from "../components/Card/DashboardCard";
+import useGetWills from "../hooks/useGetWills";
+import WillCardHeader from "../components/Card/WillCardHeader";
+import WillsCard from "../components/Card/WillsCard";
 
-const ABI = contractAbi.abi;
 
 const Dashboard = () => {
 
-    const [wills, setWills] = useState(0)
-    const [totalBalance, setTotalBalance] = useState(0)
-    const [loading, setLoading] = useState(false)
-    const [willInfo, setWillInfo] = useState(null)
-    const [hasWill, setHasWill] = useState(false)
+    const {wills, willsCreated, totalBalance, hasWill, willInfo} = useGetWills()
 
-    const {provider, contract, walletAddress} = useContract()
-
-    const fetchWills = async () => {
-      if (!provider || !walletAddress) return;
-      try {
-        setLoading(true)
-
-        const contracts = new Contract(contractAddress, ABI, provider)
-
-        const filter = contracts.filters.WillCreated()
-        const events = await contracts.queryFilter(filter, 0, 'latest')
-
-        const parsed = events.map(event => ({
-          testator: event.args.testator,
-          beneficiaries: event.args.beneficiaries,
-          amounts: event.args.amounts.map(a => ethers.utils.formatEther(a)),
-          balance: ethers.utils.formatEther(event.args.balance),
-          deathTimeout: event.args.deathTimeout.toString(),
-          blockNUmber: event.blockNumber
-        }))
-
-        const will = await contract.usersWill(walletAddress);
-        const isCreated = will?.balance.gt(0)
-        const totalEther = parsed.reduce((sum, will) => parseFloat(sum) + parseFloat(will.balance), ethers.BigNumber.from(0))
-
-        setWills(parsed.length)
-        setTotalBalance(totalEther)
-        setHasWill(isCreated)
-        setWillInfo(will)
-        
-      } catch (error) {
-        const message = error?.error?.message || error?.message || error;
-        console.error(message);
-      } finally {
-        setLoading(false)
-      }
-    }
-
-    useEffect(() => {
-      fetchWills();
-    }, [provider, contract])
   return (
     <DashboardLayout>
         <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6">
-            <DashboardCard title={'Wills Created'} value={wills} />
-            <DashboardCard title={'Total Value Locked'} value={`${totalBalance} ETH`} />
+            <DashboardCard title={'Wills Created'} value={willsCreated} />
+            <DashboardCard title={'Total Value Locked'} value={`${totalBalance.toFixed(5)} ETH`} />
             <DashboardCard title={'My Will Status'} value={hasWill ? (willInfo.executed ? 'Executed' : 'Active'): "You don't have a Will"} />
         </div>
+          <WillCardHeader>
+              {wills?.map((will, idx) => (
+                <WillsCard
+                    address={will.address}
+                    timeLeft={will.timeLeft > 0 ? will.timeLeft : 0}
+                    status={will.executed ? 'Executed' : will.isDead ? 'Dead - Can execute' : 'Alive'}
+                    balance={will.balance}
+                    lastPing={will.lastPing}
+                    deathTimeout={will.deathTimeout}
+                    cancelled={will.cancelled? 'Yes' : 'No'}
+                />
+              ))}
+          </WillCardHeader>
     </DashboardLayout>
   );
 };
