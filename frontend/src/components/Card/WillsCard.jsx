9bd4670727b1d52c8e2369ec9c40658b412017cf
abdulrahman badamasi <abdulrahmanbadamasi123@gmail.com>
2025-07-31 04:38:38 +0100

    feat: add Card component for wills dashboard

--- frontend/src/components/Card/WillsCard.jsx
+++ frontend/src/components/Card/WillsCard.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import TruncatedAddress from '../TruncatedAddress'
+import CountdownTimer from '../../hooks/CountdownTimer'
+import { ethers } from 'ethers'
 
-const WillsCard = () => {
+const WillsCard = ({deathTimeout, status, address, balance, lastPing, timeLeft, cancelled}) => {
   return (
-    <div>WillsCard</div>
+    <div className='grid grid-cols-7 p-4 mb-3 text-gray-300 rounded-lg bg-[#151515] shadow-lg'>
+        <TruncatedAddress address={address} className='col-span-2' />
+        <p>{parseFloat(ethers.utils.formatEther(balance.toString())).toFixed(5)} ETH</p>
+        <p className='col-span-2'>{status}</p>
+        {timeLeft > 0 ? 
+          <CountdownTimer lastPing={lastPing} deathTimeout={deathTimeout} /> : 
+          'Expired'
+        }
+        <p>{cancelled}</p>
+    </div>
   )
 }
 
